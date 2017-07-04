7a4a78c5b0fcae3ef098b42130870c0f5f229ec0
ducktail <tutusr708m@gmail.com>
2017-07-04 12:18:58 +0900

    alds1_1_c

---
+++ ocaml/alds1_1_c.ml
@@ -0,0 +1,20 @@
+let is_prime = function
+    1 -> false
+  | 2 -> true
+  | n when n mod 2 = 0 -> false
+  | n -> let rec iter x =
+           if x * x > n then true
+           else if n mod x = 0 then false
+           else iter (x+2) in
+         iter 3
+  
+let rec read f n =
+  if n <= 0 then []
+  else
+    let x = f () in
+    x :: read f (n - 1)
+
+let () =
+  let n = read_int () in
+  let xs = read read_int n in
+  List.filter is_prime xs |> List.length |> string_of_int |> print_endline
