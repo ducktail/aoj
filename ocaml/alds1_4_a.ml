52602515e72f0d3accd77286ac18dd61eccbf670
ducktail <tutusr708m@gmail.com>
2017-07-11 14:01:01 +0900

    alds1_4_a

---
+++ ocaml/alds1_4_a.ml
@@ -0,0 +1,11 @@
+open Str
+
+let read_list f =
+  split (regexp " +") (read_line ()) |> List.map f
+                                                 
+let () =
+  let _ = read_line () in
+  let s = read_list int_of_string in
+  let _ = read_line () in
+  let t = read_list int_of_string in
+  List.fold_left (fun c x -> if List.mem x s then c+1 else c) 0 t |> string_of_int |> print_endline
