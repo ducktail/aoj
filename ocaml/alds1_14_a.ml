5ace0c3be32a86ed203c5b8787381e37708d9430
ducktail <tutusr708m@gmail.com>
2017-08-03 12:24:53 +0900

    alds1_14_a

---
+++ ocaml/alds1_14_a.ml
@@ -0,0 +1,9 @@
+let () =
+  let t = read_line () in
+  let p = read_line () in
+  let pl = String.length p in
+  let rec loop i =
+    if String.sub t i pl = p then
+      string_of_int i |> print_endline;
+    loop (i+1) in
+  try loop 0 with _ -> ()
